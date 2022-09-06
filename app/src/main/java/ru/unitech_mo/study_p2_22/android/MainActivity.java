9e9aa42dcfbffadb92549f9d0570e8127092b092
Oleg Ignatov <ignatov.oe@ut-mo.ru>
2022-09-06 10:58:44 +0300

    INITIAL COMMIT

---
+++ app/src/main/java/ru/unitech_mo/study_p2_22/android/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.unitech_mo.study_p2_22.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
